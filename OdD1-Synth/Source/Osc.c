cf67e8ba7fd075bd4e2b72d3a2bf83e1793494f6
Sam Stratter <humanharddrive@gmail.com>
2018-05-21 21:48:35 -0400

    Remove pointer macro. Just use arrays.
    Changing all of the larger data types to byte arrays and type casting them as needed saved 16% usage.

--- OdD1-Synth/Source/Osc.c
+++ OdD1-Synth/Source/Osc.c
@@ -30,9 +30,11 @@ void Osc_Init()
 	//Initialize oscillators
 	osc1.note = osc2.note = 44;
 	osc1.waveform = osc2.waveform = WAVE_SINE;
+	osc1.wavemix = osc2.wavemix = 255;
+	osc1.wavemixnext = osc2.wavemixnext = 0;
 	osc1.phaseaccum = osc2.phaseaccum = 0;
 	osc1.phase = osc2.phase = 0;
-	osc1.level = osc2.level = 0;
+	osc1.level = osc2.level = 0x0140;
 	osc1.tuningword = osc2.tuningword = pgm_read_dword(keyFreq + osc1.note + KEY_OFFSET);
 	osc1.index = byte_addr(osc1.phaseaccum, 2);
 	osc2.index = byte_addr(osc2.phaseaccum, 2);
@@ -95,75 +97,74 @@ void Osc_ChangeLevel2(uint16_t scale)
 //69% of 510 cycles (LFO/ADSR/NOISE)
 ISR(TIMER0_OVF_vect)
 {
-	uint16_t osc1Out, osc2Out;
-	uint16_t fraction, whole;
-	int16_t mixOut;
+	uint8_t osc1Out[2], osc2Out[2];
+	uint8_t fraction[2], whole[2];
+	uint8_t mixOut[2];
 	uint32_t mixindex;
 	sbi(PORTD, PORTD5); //Timing start
-	
-	osc1Out = osc2Out = mixOut = 0;
 	
 	osc1.phaseaccum += osc1.tuningword;
 	osc2.phaseaccum += osc2.tuningword;
 	
 	//Grab osc1 waveform
 	//Reusing fraction and whole. Sue me.
-	fraction = whole = 0;
-	mixindex = analogWaveTable + (uint8_t)(*osc1.index + osc1.phase);
-	*byte_addr(fraction, 0) = pgm_read_byte(mixindex + waveformOffset[osc1.waveform]);
-	*byte_addr(whole, 0) = pgm_read_byte(mixindex + waveformOffset[osc1.waveform + 1]);
+	fraction[1] = whole[1] = 0;
+	mixindex = (int)analogWaveTable + (uint8_t)(*osc1.index + osc1.phase);
+	fraction[0] = pgm_read_byte(mixindex + waveformOffset[osc1.waveform]);
+	whole[0] = pgm_read_byte(mixindex + waveformOffset[osc1.waveform + 1]);
 	
 	//Mix waveform 1
-	fraction *= osc1.wavemix;
-	whole *= osc1.wavemixnext;
-	whole += fraction;
-	*byte_addr(osc1Out, 0) = *byte_addr(whole, 1);
+	*((uint16_t*)fraction) *= osc1.wavemix;
+	*((uint16_t*)whole) *= osc1.wavemixnext;
+	*((uint16_t*)whole) += *((uint16_t*)fraction);
+	osc1Out[0] = whole[1];
 	
 	//Scale osc1
-	fraction = *byte_addr(osc1Out, 0) * *byte_addr(osc1.level, 0);
-	whole = *byte_addr(osc1Out, 0) * *byte_addr(osc1.level, 1);
-	osc1Out = *byte_addr(fraction, 1);
-	osc1Out += *byte_addr(whole, 0);
+	*((uint16_t*)fraction) = osc1Out[0] * *byte_addr(osc1.level, 0);
+	*((uint16_t*)whole) = osc1Out[0] * *byte_addr(osc1.level, 1);
+	*((uint16_t*)osc1Out) = *byte_addr(fraction, 1);
+	*((uint16_t*)osc1Out) += *byte_addr(whole, 0);
 	
 	
 	//Grab osc2 waveform
-	fraction = whole = 0;
-	mixindex = analogWaveTable + (uint8_t)(*osc2.index + osc2.phase);
-	*byte_addr(fraction, 0) = pgm_read_byte(mixindex + waveformOffset[osc2.waveform]);
-	*byte_addr(whole, 0) = pgm_read_byte(mixindex + waveformOffset[osc2.waveform + 1]);
+	fraction[1] = whole[1] = 0;
+	mixindex = (int)analogWaveTable + (uint8_t)(*osc2.index + osc2.phase);
+	fraction[0] = pgm_read_byte(mixindex + waveformOffset[osc2.waveform]);
+	whole[0] = pgm_read_byte(mixindex + waveformOffset[osc2.waveform + 1]);
 	
 	//Mix waveform 2
-	fraction *= osc2.wavemix;
-	whole *= osc2.wavemixnext;
-	whole += fraction;
-	*byte_addr(osc2Out, 0) = *byte_addr(whole, 1);	
+	*((uint16_t*)fraction) *= osc2.wavemix;
+	*((uint16_t*)whole) *= osc2.wavemixnext;
+	*((uint16_t*)whole) += *((uint16_t*)fraction);
+	osc2Out[0] = whole[1];
 	
-	fraction = *byte_addr(osc2Out, 0) * *byte_addr(osc2.level, 0);
-	whole = *byte_addr(osc2Out, 0) * *byte_addr(osc2.level, 1);
-	osc2Out = *byte_addr(fraction, 1);
-	osc2Out += *byte_addr(whole, 0);
+	//Scale osc2
+	*((uint16_t*)fraction) = osc2Out[0] * *byte_addr(osc2.level, 0);
+	*((uint16_t*)whole) = osc2Out[0] * *byte_addr(osc2.level, 1);
+	*((uint16_t*)osc2Out) = *byte_addr(fraction, 1);
+	*((uint16_t*)osc2Out) += *byte_addr(whole, 0);
 	
 	
 	//Mix the signals
-	mixOut = osc1Out;
-	mixOut -= (osc1.level >> 1) - 1; //Offset
-	mixOut += osc2Out;
-	mixOut -= (osc2.level >> 1) - 1; //Offset
-	mixOut >>= 1;
+	*((int16_t*)mixOut) = *((uint16_t*)osc1Out);
+	*((int16_t*)mixOut) -= (osc1.level >> 1) - 1; //Offset
+	*((int16_t*)mixOut) += *((uint16_t*)osc2Out);
+	*((int16_t*)mixOut) -= (osc2.level >> 1) - 1; //Offset
+	*((int16_t*)mixOut) >>= 1;
+
+	//Restore offset
+	*((int16_t*)mixOut) += 0x7F;
 
 	//Hard clip
 	//Better way?
-	if(mixOut > 128)
-		mixOut = 127;
-	else if(mixOut < -127)
-		mixOut = -127;
-		
-	//Restore offset
-	mixOut += 0x7F;
+	if(*((int16_t*)mixOut) > 0xFF)
+		mixOut[0] = 0xFF;
+	else if(*((int16_t*)mixOut) < 0)
+		mixOut[0] = 0;
 	
-	OCR0A = *byte_addr(mixOut, 0);
+	OCR0A = mixOut[0];
 	
-	if(NextOsc1WaveReady)
+	/*if(NextOsc1WaveReady)
 	{
 		osc1.waveform = *byte_addr(NextOsc1Waveform, 1);
 		osc1.wavemix = ~*byte_addr(NextOsc1Waveform, 0);
@@ -189,7 +190,7 @@ ISR(TIMER0_OVF_vect)
 	{
 		osc2.level = NextOsc2Level;
 		NextOsc2LevelReady = 0;
-	}	
+	}*/	
 	
 	cbi(PORTD, PORTD5); //Timing stop
 }
