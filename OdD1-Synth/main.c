28c40d403819dac584cc8458590de7cc6515839e
Sam Stratter <humanharddrive@gmail.com>
2018-06-06 21:49:18 -0400

    New project file

---
+++ OdD1-Synth/main.c
@@ -0,0 +1,18 @@
+/*
+ * OdD1-Synth.c
+ *
+ * Created: 6/6/2018 9:42:12 PM
+ * Author : EmbedPC
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
